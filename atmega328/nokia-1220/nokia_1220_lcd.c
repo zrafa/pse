efe3de595167d766d6f317db3e86f5eccc760b71
Miriam Lechner <lechnermiriam@gmail.com>
2019-03-08 07:55:12 -0300

    alguna mejora en el estilo

--- atmega328/nokia-1220/nokia_1220_lcd.c
+++ atmega328/nokia-1220/nokia_1220_lcd.c
@@ -1,51 +1,27 @@
 /* Nokia 1220 LCD test */
 
+
 #include <avr/io.h>
-#define F_CPU 16000000UL
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 
-// set output pins for lcd here
-/*
-#define sclk    PORTB.0
-#define sda     PORTB.1
-#define cs      PORTB.2
-#define rst     PORTB.3
-*/
-/*
-#define SCLK_SET     { PORTC |= (1<<PC3); //asm("NOP"); }
-#define SDA_SET      { PORTC |= (1<<PC2); //asm("NOP"); }
-#define CS_SET       { PORTC |= (1<<PC1); //asm("NOP"); }
-#define RST_SET      { PORTC |= (1<<PC0); //asm("NOP"); }
-
-#define SCLK_RESET    { PORTC &= ~(1<<PC3); //asm("NOP"); }
-#define SDA_RESET     { PORTC &= ~(1<<PC2); //asm("NOP"); }
-#define CS_RESET      { PORTC &= ~(1<<PC1); //asm("NOP"); }
-#define RST_RESET     { PORTC &= ~(1<<PC0); //asm("NOP"); }
-*/
-
-/* Cableado RAFA */
-//        PCF8814(unsigned char sclk  = 13,   /* clock       (display pin 2) */
-//                unsigned char sdin  = 11,   /* data-in     (display pin 3) */
-//                unsigned char reset = 12,   /* reset       (display pin 8) */
-//                unsigned char sce   = 10);  /* enable      (display pin 5) */
+#define F_CPU 16000000UL
 
 
-/* RAFA pinout 1220
-nokia pin
-
-
-1 _RESET 	12
-2 _CS		10
-3 Vss		GND
-4 DATA		11
-5 CLOCK		13
-6 Vlogic	3.3v
-7 Vsupply	3.3v
-8 VLCD (out)    capacitor 0.1uf a GND
-
-*/
-
+/* RAFA pinout 1220 (PORTB)
+ * 
+ * nokia pin       avr pin (atenti: arduino pin)
+ * 
+ * 1 _RESET 	12
+ * 2 _CS		10
+ * 3 Vss		GND
+ * 4 DATA		11
+ * 5 CLOCK		13
+ * 6 Vlogic	3.3v
+ * 7 Vsupply	3.3v
+ * 8 VLCD (out)    capacitor 0.1uf a GND
+ * 
+ */
 
 #define SCLK_SET     PORTB |= (1<<PB5)
 #define SDA_SET      PORTB |= (1<<PB3)
@@ -60,21 +36,22 @@ nokia pin
 #define CMD     0
 #define DATA    1
 
-void Lcd_Clear(void);
-void Lcd_Init(void);
-void Lcd_Write(char cd,unsigned char c);
-void SetX(char x);
-void SetY(char y);
-void Gotoxy(char x,char y);
+void lcd_clear(void);
+void lcd_init(void);
+void lcd_write(char cd,unsigned char c);
+void set_x(char x);
+void set_y(char y);
+void goto_xy(char x,char y);
 void VLine(char x,char y,char on);
 void Line(unsigned char x,unsigned char y,unsigned char y2,unsigned char on);
 void print_char(char c);
-//void print_message(char * message PROGMEM);
 void print_string(char * message);
 
 char bitaddr;
-// lookup table, here you can modify the font
-//const uint8_t backslash[] PROGMEM
+
+
+/* lookup table, here you can modify the font */
+
 const unsigned char FontLookup[96][5] PROGMEM ={
 {0x00, 0x00, 0x00, 0x00, 0x00},// (space)
 {0x00, 0x00, 0x5F, 0x00, 0x00},// !
@@ -175,49 +152,47 @@ const unsigned char FontLookup[96][5] PROGMEM ={
 };
 
 void main(void)
-  {
-  // MCUCSR = 0x80;
-  // MCUCSR = 0x80;
-  MCUCR = 0x80;
-  MCUCR = 0x80;
-  DDRB=0xFF;
-  Lcd_Init();
-  _delay_ms(100);
-  Gotoxy(0,0);
-  _delay_ms(100);
-  print_string("RAFA RAFA !!  RAFA of Nokia     1100 LCD ..");
-  _delay_ms(100);
-  Gotoxy(20,5);
-  _delay_ms(1000);
-  print_string("It works!!");
-  _delay_ms(1000);
- 
-  while(1)
-  {
-    print_string("FUNCIONA!!");
-    _delay_ms(1000);
-  }
-  }
+{
+	MCUCR = 0x80;
+	MCUCR = 0x80;
 
-// clear LCD
-void Lcd_Clear(void){
-unsigned int i;
-Lcd_Write(CMD,0x40); // Y = 0
-Lcd_Write(CMD,0xb0);
-Lcd_Write(CMD,0x10); // X = 0
-Lcd_Write(CMD,0x00);
-//Lcd_Write(CMD,0xae); // disable display;
-for(i=0;i<864;i++)
-Lcd_Write(DATA,0x00);
-//Lcd_Write(CMD,0x07);
-//Lcd_Write(CMD,0xaf); // enable display;
+	DDRB=0xFF;
+
+	lcd_init();
+
+	_delay_ms(100);
+	goto_xy(0,0);
+	_delay_ms(100);
+	print_string("RAFA RAFA !!  RAFA of Nokia  1220 LCD ..");
+	_delay_ms(100);
+	goto_xy(20,5);
+	_delay_ms(1000);
+	print_string("It works!!");
+	_delay_ms(1000);
+ 
+	while(1) {
+    		print_string("FUNCIONA!!");
+		_delay_ms(1000);
+  	}
+}
+
+/* clear LCD */
+void lcd_clear(void)
+{
+	unsigned int i;
+
+	lcd_write(CMD,0x40); /* Y = 0 */
+	lcd_write(CMD,0xb0);
+	lcd_write(CMD,0x10); /* X = 0 */
+	lcd_write(CMD,0x00);
+
+	for(i=0;i<864;i++)
+		lcd_write(DATA,0x00);
 }
 
 
 
-
-
-
+/* Rafa Init para pcf8511. Obtenido de serdisplay.sourceforge.net */
 
 #define LCD_NOP 0xE3
 #define LCD_MODE 0xA0
@@ -242,238 +217,230 @@ Lcd_Write(DATA,0x00);
 #define DISPLAY 0x0E
 
 
-// init LCD
-void Lcd_Init(void){
-//RST_RESET;
-CS_SET;
-SCLK_RESET;
+/* init LCD */
+void lcd_init(void)
+{
 
-RST_RESET;
-_delay_ms(1200);
-RST_SET;
+	CS_SET;
+	SCLK_RESET;
 
-/* obtenido de pcf8511 de serdisplay.sf.net */
+	RST_RESET;
+	_delay_ms(1200);
+	RST_SET;
 
-    Lcd_Write(CMD, 0xE1);               /* exit powersave */
-    Lcd_Write(CMD, 0x2F);               /* hvgen on */
-    Lcd_Write(CMD, 0x81);               /* set vop: */
-    Lcd_Write(CMD, 0x3F);               /* vop */
-    Lcd_Write(CMD, 0x20+5);             /* set voff */
-    Lcd_Write(CMD, 0x64+3);             /* set mult fact */
-    Lcd_Write(CMD, 0xA4);               /* no all pixels on mode */
-    Lcd_Write(CMD, 0xAF);               /* display on */
-    Lcd_Write(CMD, 0xAF);
+	/* obtenido de pcf8511 de serdisplay.sf.net */
 
-/* ahora contraste y brillo */
-      Lcd_Write(CMD, 0x45); /* NOP */
-      Lcd_Write(CMD, 0x00); /* NOP */
+	lcd_write(CMD, 0xE1);               /* exit powersave */
+	lcd_write(CMD, 0x2F);               /* hvgen on */
+	lcd_write(CMD, 0x81);               /* set vop: */
+	lcd_write(CMD, 0x3F);               /* vop */
+	lcd_write(CMD, 0x20+5);             /* set voff */
+	lcd_write(CMD, 0x64+3);             /* set mult fact */
+	lcd_write(CMD, 0xA4);               /* no all pixels on mode */
+	lcd_write(CMD, 0xAF);               /* display on */
+	lcd_write(CMD, 0xAF);
 
-      Lcd_Write(CMD, 0x81); /* set vop: */
-      Lcd_Write(CMD, 0x3F); /* vop */
-      Lcd_Write(CMD, 0x20 + 99); /*dd->curr_contrast*/
+	/* ahora contraste y brillo */
+        /* lcd_write(CMD, 0x45);  */ /* NOP */ 
+	lcd_write(CMD, 0x00); /* NOP */
+
+	lcd_write(CMD, 0x81); /* set vop: */
+	lcd_write(CMD, 0x3F); /* vop */
+	// lcd_write(CMD, 0x20 + 99); /*dd->curr_contrast*/
+	lcd_write(CMD, 0x7F); /*dd->curr_contrast*/
 
 
 
 /*
-Lcd_Write(CMD, 0x21);     
-Lcd_Write(CMD, 0xC8);    
-Lcd_Write(CMD, 0x06);    
-Lcd_Write(CMD, 0x13);   
-Lcd_Write(CMD, 0x20);  
-Lcd_Write(CMD, 0x0c);     
+lcd_write(CMD, 0x21);     
+lcd_write(CMD, 0xC8);    
+lcd_write(CMD, 0x06);    
+lcd_write(CMD, 0x13);   
+lcd_write(CMD, 0x20);  
+lcd_write(CMD, 0x0c);     
 _delay_us(500);
 */
 
 /*
 
-Lcd_Write(CMD,0b10100000); 
-Lcd_Write(CMD,0b00000000); 
-Lcd_Write(CMD,0b00010000); 
-Lcd_Write(CMD,0b10110000); 
-Lcd_Write(DATA,0b00000000); 
+lcd_write(CMD,0b10100000); 
+lcd_write(CMD,0b00000000); 
+lcd_write(CMD,0b00010000); 
+lcd_write(CMD,0b10110000); 
+lcd_write(DATA,0b00000000); 
 
 
-Lcd_Write(CMD,0xe2); 
-Lcd_Write(CMD,0xa0); 
-Lcd_Write(CMD,0xc0); 
-Lcd_Write(CMD,0xa4); 
-Lcd_Write(CMD,0x2f); 
-Lcd_Write(CMD,0xaf); 
+lcd_write(CMD,0xe2); 
+lcd_write(CMD,0xa0); 
+lcd_write(CMD,0xc0); 
+lcd_write(CMD,0xa4); 
+lcd_write(CMD,0x2f); 
+lcd_write(CMD,0xaf); 
 
-Lcd_Write(CMD, LCD_VOB_MSB|0x04);
-Lcd_Write(CMD, LCD_VOB_LSB|(0x05 & 0x1F));
+lcd_write(CMD, LCD_VOB_MSB|0x04);
+lcd_write(CMD, LCD_VOB_LSB|(0x05 & 0x1F));
 
-        Lcd_Write(CMD, LCD_MODE|DISPLAY|ON);
-        Lcd_Write(CMD, LCD_MODE|ALL|OFF);
-        Lcd_Write(CMD, LCD_MODE|INVERT|OFF);
+        lcd_write(CMD, LCD_MODE|DISPLAY|ON);
+        lcd_write(CMD, LCD_MODE|ALL|OFF);
+        lcd_write(CMD, LCD_MODE|INVERT|OFF);
 
 */
-_delay_ms(200);            // 5mS so says the stop watch(less than 5ms will not work)
+	_delay_ms(200);            // 5mS so says the stop watch(less than 5ms will not work)
 
-
-// Lcd_Clear();
-
-// Lcd_Write(CMD,LCD_CHARGE_PUMP_ON); 
-
+// lcd_clear();
+// lcd_write(CMD,LCD_CHARGE_PUMP_ON); 
 
 }
 
 
-void Lcd_Write(char cd,unsigned char c)
+void lcd_write(char cd,unsigned char c)
 {
-    unsigned char i;
-    CS_RESET;
-   
-    SCLK_RESET;
-   
-        if(cd)
-            {
-                SDA_SET;
-            }
-        else
-            {
-                SDA_RESET;
-            }
-       
-    SCLK_SET;
+	unsigned char i;
 
-        for(i=0;i<8;i++)
-            {
-            SCLK_RESET;
-                if(c & 0x80)
+	CS_RESET;
+   
+	SCLK_RESET;
+   
+	if (cd)
+	{
+		SDA_SET;
+	}
+	else
+	{
+		SDA_RESET;
+	}
+       
+	SCLK_SET;
+
+	for (i=0;i<8;i++) {
+		SCLK_RESET;
+		if (c & 0x80)
                     {
                         SDA_SET;
                     }
-                else
+		else
                     {
                         SDA_RESET;
                     }
-            SCLK_SET;
-            c <<= 1;
-            _delay_ms(3);
-            }
-    CS_SET;
+		SCLK_SET;
+		c <<= 1;
+		_delay_ms(3);
+	}
+	CS_SET;
 }
 
 
-void SetX(char x){
-    Lcd_Write(CMD,0x10 | ((x>>4)&0x7));
-    Lcd_Write(CMD,x & 0x0f);
+void set_x(char x) 
+{
+	lcd_write(CMD,0x10 | ((x>>4)&0x7));
+	lcd_write(CMD,x & 0x0f);
 }
 
-void SetY(char y){
-    char aux = 0;
-    while(y>7){
-        y -=8;
-        aux ++;
-    }
+void set_y(char y) 
+{
+	char aux = 0;
+
+	while (y > 7) {
+		y -=8;
+		aux ++;
+	}
   
-    bitaddr = y-1;
-    Lcd_Write(CMD,0xB0 | (aux & 0x0f));
+	bitaddr = y-1;
+	lcd_write(CMD,0xB0 | (aux & 0x0f));
 }
 
-void Gotoxy(char x,char y){
-        Lcd_Write(CMD,(0xB0|(y&0x0F)));         // Y axis initialisation: 0100 yyyy          
-        Lcd_Write(CMD,(0x00|(x&0x0F)));         // X axis initialisation: 0000 xxxx ( x3 x2 x1 x0)
-        Lcd_Write(CMD,(0x10|((x>>4)&0x07))); // X axis initialisation: 0010 0xxx  ( x6 x5 x4)
+void goto_xy(char x,char y)
+{
+	lcd_write(CMD,(0xB0|(y&0x0F)));         // Y axis initialisation: 0100 yyyy          
+	lcd_write(CMD,(0x00|(x&0x0F)));         // X axis initialisation: 0000 xxxx ( x3 x2 x1 x0)
+	lcd_write(CMD,(0x10|((x>>4)&0x07))); // X axis initialisation: 0010 0xxx  ( x6 x5 x4)
 
 }
 
 
-//x dir >    y dir from last line to ^
-void VLine(char x,char y,char on){
-    char aux[8];
-    char i;
+/* x dir    >    y dir from last line to ^ */
+void VLine(char x,char y,char on) {
+	char aux[8];
+	char i;
 
-    for(i=0;i<8;i++)
-        aux[i] = 0;
+	for(i=0;i<8;i++)
+		aux[i] = 0;
       
-    i = 7;
-    while(y > 9){
-        aux[i] = 0xff;
-        y -= 8;
-        i--;
-    }
+	i = 7;
+	while(y > 9) {
+		aux[i] = 0xff;
+		y -= 8;
+		i--;
+	}
   
-    while(y > 0){
-        aux[i] >>= 1;
-        aux[i] |= 0x80;
-        y --;
-    }  
+	while(y > 0) {
+		aux[i] >>= 1;
+		aux[i] |= 0x80;
+		y --;
+	}  
   
 
   
-    for(y=0;y<8;y++){
-        Lcd_Write(CMD,0xB0 | y);
-        SetX(x);      
-        Lcd_Write(DATA,aux[y]);
-    }  
+	for(y=0;y<8;y++) {
+		lcd_write(CMD,0xB0 | y);
+		set_x(x);      
+		lcd_write(DATA,aux[y]);
+	}  
       
 }
 
-// xdir is >       y is always < than y2  can be used for graphs
-void Line(unsigned char x,unsigned char y,unsigned char y2,unsigned char on){
-    char aux[8];
-    char i,o=0;
-    char c=1;
-    char index;
+/* xdir is >       y is always < than y2  can be used for graphs */
+void Line(unsigned char x,unsigned char y,unsigned char y2,unsigned char on) {
+	char aux[8];
+	char i,o=0;
+	char c=1;
+	char index;
   
-
-  
-    for(i=0;i<8;i++)
-        aux[i] = 0;
+	for(i=0;i<8;i++)
+		aux[i] = 0;
       
-
-    index = 0;
-    for(i=0;i<64;i++){
+	index = 0;
+	for(i=0;i<64;i++) {
       
-        if(i >= y && i <= y2){
-            aux[index] |= c << o;
-        }
+		if (i >= y && i <= y2) {
+			aux[index] |= c << o;
+		}
       
-        o ++;
-        if(o == 8){
-            o = 0;
-            index ++;
-        }
-    }
+		o++;
+		if (o == 8) {
+			o = 0;
+			index ++;
+		}
+	}
           
-  
-    for(i=0;i<8;i++){
-        Lcd_Write(CMD,0xB0 | i);
-        SetX(x);      
-        Lcd_Write(DATA,aux[i]);
-    }
-  
+	for(i=0;i<8;i++){
+		lcd_write(CMD,0xB0 | i);
+		set_x(x);      
+		lcd_write(DATA,aux[i]);
+	}
   
 }
     
 
       
-void print_char(char c){
-    int i;
+void print_char(char c)
+{
+	int i;
 
-            for ( i = 0; i < 5; i++ ){
-                Lcd_Write(DATA,(pgm_read_byte(&FontLookup[c - 32][i]) << 1));  //pgm_read_byte(&backslash[i]);
-        }
-            Lcd_Write(DATA,0x00);
+	for (i = 0; i < 5; i++) {
+		lcd_write(DATA,(pgm_read_byte(&FontLookup[c - 32][i]) << 1));  //pgm_read_byte(&backslash[i]);
+	}
+	lcd_write(DATA,0x00);
 
 }
 
-/*
-void print_message(flash char * message){    // Write message to LCD (C string type)
-Lcd_Write(CMD,0xae); // disable display;
-        while (*message){            // Look for end of string
-                    print_char(*message++);
-                    }
-    Lcd_Write(CMD,0xaf); // enable display;
-                        }*/
-
-void print_string(char * message){    // Write message to LCD (C string type)
-//Lcd_Write(CMD,0xae); // disable display;
-        while (*message){            // Look for end of string
-                    print_char(*message++);
-                    }
- //   Lcd_Write(CMD,0xaf); // enable display;
-                        }
+/* Write message to LCD (C string type) */
+void print_string (char * message)
+{    
+	//lcd_write(CMD,0xae); // disable display;
+        while (*message) {            /* Look for end of string */
+		print_char(*message++);
+	}
+	//   lcd_write(CMD,0xaf); // enable display;
+}
 
