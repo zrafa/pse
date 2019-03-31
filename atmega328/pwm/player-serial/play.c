329485f9c43f67111bb592dd2105aace31245ba9
Miriam Lechner <lechnermiriam@gmail.com>
2019-03-31 07:28:14 -0300

    serial init

--- atmega328/pwm/player-serial/play.c
+++ atmega328/pwm/player-serial/play.c
@@ -1,15 +1,16 @@
 /*
-  ATmega32 @ 8MHz
-  Plays an 8bit/8000 sample PCM audio on OC1A output
-*/
+ * Ejemplo de PWM y timer con interrupciones
+ *
+ * Reproduce un wav de 11000 hz, 8bit, obtenido via uart a 115200
+ * en la salida OC1A 
+ */
 
-#include <stdint.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
-// #include "pcm_sample_2.h"
-#include "serial.h"
 #include <avr/interrupt.h>
-#define SAMPLE_RATE 8000;
+#include <stdint.h>
+
+#include "serial.h"
 
 volatile uint16_t sample;
 int sample_count;
@@ -25,7 +26,7 @@ void pwm_init(void)
     /*
     * clear OC1A on compare match
     * set OC1A at BOTTOM, non-inverting mode
-    * Fast PWM, 8bit
+    * Fast PWM
     */
     TCCR1A = _BV(COM1A1) | _BV(WGM11);
    
@@ -55,17 +56,19 @@ volatile unsigned int pcm_length;
 ISR(TIMER1_OVF_vect)
 {
 	/* Cada dos interrupciones enviamos un nuevo valor (duty cycle)
- 	 * Esto permite hacer 8000 samples por segundo 8Mhz
+ 	 * Estamos haciendo 16000 interrupciones por segundo
 	 */
 	sample_count--;
 	if (sample_count == 0) {   
 
-		// OCR1A = music[sample];
+		/* OCR1A = music[sample]; */
 		OCR1A = serial_get_char();
 
+/*
 		sample++;
 		if (sample > pcm_length) 	
 			sample=0;
+*/
 
 		sample_count = 2;
 	}
@@ -77,19 +80,23 @@ int main(void)
 {
 	int i;
 
-    DDRB = 0xFF;
-    PORTB &= ~(1 << PB5);
+	DDRB = 0xFF;
+	PORTB &= ~(1 << PB5);
 
+/* NO utilizo buffer por ahora */
 	serial_init();
+/*
 	for (i=0; i<1400; i++)
 		music[i] = serial_get_char();
-   
 	pcm_length = 1500;
+   
+*/
 	pwm_init();
 
-    PORTB |= (1 << PB5);
-    //PORTB &= ~(1 << PB5);
+	PORTB |= (1 << PB5);
 	while(1) {
+
+/* NO utilizo buffer por ahora */
 /*
 		i++;
 		if (i == 1500) i = 0;
@@ -98,6 +105,6 @@ int main(void)
 	};
 
 
-;//do nothing
+
 }
 
